df4b12afaf62049e5892bab79db91ed71598868e
sree121999 <sree91336@gmail.com>
2025-07-23 15:43:33 +0530

    Fix: resolved conflict in vite.config.js with Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 21876939a92dd38b1e8a883d65f2b6b42d330217
+  plugins: [react(), tailwindcss()],
 })
